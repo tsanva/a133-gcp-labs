4fecff4517e82bfb9593192b6bd1ee508e45cf35
vasant paradissa <vasantpns@gmail.com>
2022-08-28 21:17:43 +0700

    remove index

--- application/config/config.php
+++ application/config/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = 'https://dicoding.vasant.xyz';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
